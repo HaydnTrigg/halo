b1f1defc5496358a713aab537a6406f16bd6548a
punpckhdq <121350336+punpckhdq@users.noreply.github.com>
2025-12-30 20:59:48 +0000

    Add more real math types

--- source/math/real_math.h
+++ source/math/real_math.h
@@ -16,12 +16,27 @@ file has inline function assertions.
 
 typedef float real;
 
+union real_euler_angles2d
+{
+	real n[2];
+	struct { real yaw; real pitch; };
+};
+typedef union real_euler_angles2d real_euler_angles2d;
+
 union real_euler_angles3d
 {
 	real n[3];
 	struct { real yaw, pitch, roll; };
 };
 typedef union real_euler_angles3d real_euler_angles3d;
+
+union real_point2d
+{
+	real n[2];
+	struct { real x; real y; };
+	struct { real u; real v; };
+};
+typedef union real_point2d real_point2d;
 
 union real_point3d
 {
@@ -31,12 +46,101 @@ union real_point3d
 };
 typedef union real_point3d real_point3d;
 
+union real_vector2d
+{
+	real n[2];
+	struct { real i; real j; };
+};
+typedef union real_point2d real_point2d;
+
 union real_vector3d
 {
 	real n[3];
 	struct { real i, j, k; };
 };
 typedef union real_vector3d real_vector3d;
+
+union real_vector4d
+{
+	real n[4];
+	struct { real i; real j; real k; real l; };
+};
+typedef union real_vector4d real_vector4d;
+
+struct real_quaternion
+{
+	real_vector3d v;
+	real w;
+};
+typedef struct real_quaternion real_quaternion;
+
+struct real_orientation
+{
+	real_quaternion rotation;
+	real_point3d translation;
+	real scale;
+};
+typedef struct real_orientation real_orientation;
+
+struct real_matrix3x3
+{
+	union
+	{
+		real n[3][3];
+		struct
+		{
+			real_vector3d forward;
+			real_vector3d left;
+			real_vector3d up;
+		};
+	};
+};
+typedef struct real_matrix3x3 real_matrix3x3;
+
+struct real_matrix4x3
+{
+	real scale;
+	union
+	{
+		real n[4][3];
+		struct
+		{
+			real_vector3d forward;
+			real_vector3d left;
+			real_vector3d up;
+			real_point3d position;
+		};
+	};
+};
+typedef struct real_matrix4x3 real_matrix4x3;
+
+struct real_plane2d
+{
+	real_vector2d n;
+	real d;
+};
+typedef struct real_plane2d real_plane2d;
+
+struct real_plane3d
+{
+	real_vector3d n;
+	real d;
+};
+typedef struct real_plane3d real_plane3d;
+
+union real_rectangle2d
+{
+	real n[4];
+	struct { real x0; real x1; real y0; real y1; };
+};
+typedef union real_rectangle2d real_rectangle2d;
+
+union real_rectangle3d
+{
+	real n[6];
+	struct { real x0; real x1; real y0; real y1; real z0; real z1; };
+};
+typedef union real_rectangle3d real_rectangle3d;
 
 union real_rgb_color
 {
