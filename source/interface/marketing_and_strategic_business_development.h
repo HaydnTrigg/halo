7d03644b33acdb1b140771472603400722a0641a
punpckhdq <121350336+punpckhdq@users.noreply.github.com>
2026-01-04 15:11:17 +0000

    Xdemos header cleanup

--- source/interface/marketing_and_strategic_business_development.h
+++ source/interface/marketing_and_strategic_business_development.h
@@ -1,27 +1,15 @@
 /*
 MARKETING_AND_STRATEGIC_BUSINESS_DEVELOPMENT.H
-
-header included in hcex build.
 */
 
 #ifndef __MARKETING_AND_STRATEGIC_BUSINESS_DEVELOPMENT_H
 #define __MARKETING_AND_STRATEGIC_BUSINESS_DEVELOPMENT_H
 #pragma once
 
-/* ---------- constants */
-
-/* ---------- macros */
-
-/* ---------- structures */
-
 /* ---------- prototypes/MARKETING_AND_STRATEGIC_BUSINESS_DEVELOPMENT.C */
 
 boolean xbox_demos_available(void);
 void xbox_demos_launch(void);
 void xbox_dashboard_launch(void);
-
-/* ---------- globals */
-
-/* ---------- public code */
 
 #endif // __MARKETING_AND_STRATEGIC_BUSINESS_DEVELOPMENT_H
